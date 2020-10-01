2d80777fefda7338187e6153f4ab5bcbc0ba0b5c
kratikakk <72157581+kratikakk@users.noreply.github.com>
2020-10-02 02:19:38 +0530

    Create ChangeDP.java

---
+++ week5/starterfiles/ChangeDP.java
@@ -0,0 +1,42 @@
+public class ChangeDP {
+    private static int getChange(int m) {
+        //write your code here
+        return m / 4;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        System.out.println(getChange(m));
+
+    }
+}
+import java.util.*;
+
+public class PrimitiveCalculator {
+    private static List<Integer> optimal_sequence(int n) {
+        List<Integer> sequence = new ArrayList<Integer>();
+        while (n >= 1) {
+            sequence.add(n);
+            if (n % 3 == 0) {
+                n /= 3;
+            } else if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+            }
+        }
+        Collections.reverse(sequence);
+        return sequence;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Integer> sequence = optimal_sequence(n);
+        System.out.println(sequence.size() - 1);
+        for (Integer x : sequence) {
+            System.out.print(x + " ");
+        }
+    }
+}
